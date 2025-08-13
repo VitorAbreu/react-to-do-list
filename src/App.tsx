7416522a93b50cadab9e99a49b3f159c47654507
Vitor <vitoryanfma@gmail.com>
2025-08-13 20:11:16 -0300

    feat(componentes): Criando estrutura do component Badge e Button, criando variantes de componente

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import PencilIcon from "./assets/icons/Pencil.svg?react";
 import PlusIcon from "./assets/icons/Plus.svg?react";
 import SpinnerIcon from "./assets/icons/Spinner.svg?react";
 import XIcon from "./assets/icons/X.svg?react";
+import Badge from "./components/badge";
+import Button from "./components/button";
 
 export default function App() {
   return (
@@ -25,8 +27,15 @@ export default function App() {
         <Icon svg={CheckIcon} />
         <Icon svg={PencilIcon} />
         <Icon svg={PlusIcon} />
-        <Icon svg={SpinnerIcon} />
+        <Icon svg={SpinnerIcon} animate />
         <Icon svg={XIcon} />
+      </div>
+      <div className="flex gap-1">
+        <Badge variant={"secondary"}>5</Badge>
+        <Badge variant={"primary"}>2 de 5</Badge>
+      </div>
+      <div className="flex gap-1">
+        <Button icon={PlusIcon}>Nova tarefa</Button>
       </div>
     </div>
   );
