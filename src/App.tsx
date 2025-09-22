1549997f553e7e4d6827bddba34174c1bbb36e79
Vitor <vitoryanfma@gmail.com>
2025-09-22 14:04:01 -0300

    feat(router, pages): criando rotas com reactRouter, core-componentes, pages, criando layout, e home da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,17 @@
-import Icon from "./components/icon";
-import Text from "./components/text";
-// import TrashIcon from "./assets/icons/Trash.svg" normalmente importaria assim
-// mas para tratar o svg como um componente e torna-lo mais customizavel usamos o
-// plugin svgr e usamos ?react no final para torna-lo um component
-// para padronizar e componentizar os icones criamos um Component icone que recebe
-// o component svg
-import TrashIcon from "./assets/icons/Trash.svg?react";
-import CheckIcon from "./assets/icons/Check.svg?react";
-import PencilIcon from "./assets/icons/Pencil.svg?react";
-import PlusIcon from "./assets/icons/Plus.svg?react";
-import SpinnerIcon from "./assets/icons/Spinner.svg?react";
-import XIcon from "./assets/icons/X.svg?react";
-import Badge from "./components/badge";
-import Button from "./components/button";
-import ButtonIcon from "./components/button-icon";
-import InputText from "./components/input";
-import Card from "./components/card";
-import InputCheckbox from "./components/input-checkbox";
-import Container from "./components/container";
-import Skeleton from "./components/skeleton";
+import { BrowserRouter, Route, Routes } from "react-router";
+import PageComponents from "./pages/page-components";
+import LayoutMain from "./pages/layout-main";
+import PageHome from "./pages/page-home";
 
 export default function App() {
   return (
-    <Container>
-      <div className="grid gap-3">
-        <div className="flex flex-col gap-1">
-          <Text variant="body-sm-bold" className="text-pink-base">
-            Olá Mundo
-          </Text>
-        </div>
-        <div className="flex gap-1">
-          <Icon svg={TrashIcon} />
-          <Icon svg={CheckIcon} />
-          <Icon svg={PencilIcon} />
-          <Icon svg={PlusIcon} />
-          <Icon svg={SpinnerIcon} animate />
-          <Icon svg={XIcon} />
-        </div>
-        <div className="flex gap-1">
-          <Badge variant={"secondary"}>5</Badge>
-          <Badge variant={"primary"}>2 de 5</Badge>
-          <Badge loading>a</Badge>
-        </div>
-        <div className="flex gap-1">
-          <Button icon={PlusIcon}>Nova tarefa</Button>
-        </div>
-        <div className="flex gap-1">
-          <ButtonIcon icon={PlusIcon} />
-          <ButtonIcon icon={PlusIcon} variant="secondary" />
-          <ButtonIcon icon={PlusIcon} variant="tertiary" />
-        </div>
-        <div className="flex gap-1">
-          <InputText />
-        </div>
-        <div className="flex gap-1">
-          <Card size="md">Olá mundo</Card>
-        </div>
-        <div className="flex gap-1">
-          <InputCheckbox />
-
-          <InputCheckbox loading />
-        </div>
-        <div className="space-y-2">
-          <Skeleton className="h-4" />
-          <Skeleton className="h-4" />
-          <Skeleton className="h-4 w-96" />
-        </div>
-      </div>
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
+          <Route path="/componentes" element={<PageComponents />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/core-components/task-item.tsx
@@ -0,0 +1,53 @@
+import ButtonIcon from "../components/button-icon";
+import Card from "../components/card";
+import InputCheckbox from "../components/input-checkbox";
+import Text from "../components/text";
+import TrashIcon from "../assets/icons/Trash.svg?react";
+import PencilIcon from "../assets/icons/Pencil.svg?react";
+import XlIcon from "../assets/icons/X.svg?react";
+import CheckIcon from "../assets/icons/Check.svg?react";
+import React from "react";
+import InputText from "../components/input";
+
+export default function TaskItem() {
+  const [isEditing, setIsEditing] = React.useState(false);
+
+  function handleEditTask() {
+    setIsEditing(true);
+  }
+
+  function handleExitEditTask() {
+    setIsEditing(false);
+  }
+
+  return (
+    <Card size="md" className="flex items-center gap-3">
+      {!isEditing ? (
+        <>
+          <InputCheckbox />
+          <Text className="flex-1">Fazer compras da semana</Text>
+          <div className="flex gap-1">
+            <ButtonIcon icon={TrashIcon} variant="tertiary" />
+            <ButtonIcon
+              icon={PencilIcon}
+              variant="tertiary"
+              onClick={handleEditTask}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <InputText className="flex-1" />
+          <div className="flex gap-1">
+            <ButtonIcon
+              icon={XlIcon}
+              variant="secondary"
+              onClick={handleExitEditTask}
+            />
+            <ButtonIcon icon={CheckIcon} variant="primary" />
+          </div>
+        </>
+      )}
+    </Card>
+  );
+}
